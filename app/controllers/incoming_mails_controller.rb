0615769c3166b956614aefbfff1f2cb371b331ea
Keith Mc Donnell <keith@dancingtext.com>
2011-06-14 14:30:34 +0100

    forgot to add

--- app/controllers/incoming_mails_controller.rb
+++ app/controllers/incoming_mails_controller.rb
@@ -1,9 +1,11 @@
 class IncomingMailsController < ApplicationController    
   require 'mail'
+  
   skip_before_filter :verify_authenticity_token
 
   def create
     message = Mail.new(params[:message])
+    
     Rails.logger.log message.subject #print the subject to the logs
     Rails.logger.log message.body.decoded #print the decoded body to the logs
     Rails.logger.log message.attachments.first.inspect #inspect the first attachment
