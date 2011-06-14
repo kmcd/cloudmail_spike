687febe1bdc221c540e05e1fdddd664cb5a3a5d2
Keith Mc Donnell <keith@dancingtext.com>
2011-06-14 12:04:02 +0100

    start

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cloudmail::Application.config.secret_token = '898abfdabb723084726a7368351297c79c0caf8b8812766acf682b61a8f20ce991719dd5c7aaf0beac4adfdc82c634252c7ca8b8aef813c4ff6e97c28d5b87b2'
