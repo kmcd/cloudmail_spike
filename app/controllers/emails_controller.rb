8ef2c76b82eb6815b914a2cf14efbd979c96522b
Keith Mc Donnell <keith@dancingtext.com>
2011-06-15 14:52:45 +0100

    reply to an email

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -2,4 +2,8 @@ class EmailsController < ApplicationController
   def index
     @emails = Email.all
   end
+  
+  def show
+    @email = Email.find params[:id]
+  end
 end
--- app/models/email.rb
+++ app/models/email.rb
@@ -1,2 +1,25 @@
 class Email < ActiveRecord::Base
+  def to_param
+    [ id, subject ].join '-'
+  end
+  
+  def to_text
+    message_parts.first
+  end
+  
+  def to_html
+    message_parts.last
+  end
+  
+  def message_parts()
+    message.split(/\n/).reject {|line| line =~ /^(--|Content-Type|$)/ }
+  end
+  
+  def reply_to
+    "647b9c8e2c179d02a0db+#{to_param}@cloudmailin.net"
+  end
+  
+  def replies
+    Email.where :to => reply_to
+  end
 end
